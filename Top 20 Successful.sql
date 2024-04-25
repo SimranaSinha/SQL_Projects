4f8ce73f3e699a4ade8e093dd3a7fd0c24cee790
Simran Sinha <161783369+SimranaSinha@users.noreply.github.com>
2024-04-25 19:56:32 -0400

    Create Top 20 Successful.sql

---
+++ Top 20 Successful.sql
@@ -0,0 +1,14 @@
+use Portfolio_prj1;
+
+SELECT * 
+FROM listings;
+
+
+#How does the availability of listings impact their projected revenue for the next 30 days
+#Top 20 most successful
+SELECT id, listing_url, 30 - availability_30 AS booked_out_30,
+CAST(REPLACE(Price,'$','') AS UNSIGNED) AS price_clean,
+CAST(REPLACE(Price,'$','') AS UNSIGNED)*(30 - availability_30) AS proj_rev_30
+FROM listings
+ORDER BY proj_rev_30 DESC LIMIT 20;
+
